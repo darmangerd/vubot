4d4b386d15ea3806c12d801a02896c6774bc4a59
sophiecaroni <sophie.caroni@gmail.com>
2024-05-28 23:41:54 +0200

    Remove model comparison; add single model evaluation
    Also clean code and complete dosctrings

--- accuracy_evaluation.py
+++ accuracy_evaluation.py
@@ -1,10 +1,12 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats import ttest_rel
+from scipy.stats import ttest_1samp
 
 
 def import_data(path):
     """
-    Import data from a CSV file and reset index.
+    Import data recorded during the evaluation trials from the CSV file.
     :param path: str, path to the CSV file
     :return: DataFrame, imported data
     """
@@ -13,9 +15,9 @@ def import_data(path):
 
 def add_error_column(df):
     """
-    Create a column indicating if the response was an error or not.
+    Create a column indicating if the value on the same row and in the response column was an error or not.
     :param df: DataFrame, input data
-    :return: Series, column with boolean values for error responses
+    :return: Series, column with boolean values informative of the error responses
     """
     return df['response'].apply(lambda x: x.startswith('error'))
 
@@ -23,13 +25,11 @@ def add_error_column(df):
 def accuracy_evaluation_df(path="utils/main_evaluation_accuracy.csv"):
     """
     Load and preprocess the accuracy evaluation data.
-    :param path: str, path to the CSV file
+    :param path: str, path to the CSV file of the data recorded during trials
     :return: DataFrame, preprocessed data with error column added
     """
     df = import_data(path)
     df['error'] = add_error_column(df)
-
-    # Drop columns not needed in evaluation accuracy
     df.drop(columns=['timelog'], inplace=True)
     return df
 
@@ -37,102 +37,116 @@ def accuracy_evaluation_df(path="utils/main_evaluation_accuracy.csv"):
 def compute_tasks_accuracy(df):
     """
     Calculate the number of queries, errors, and accuracy percentage by participant and by task version.
-    :param df: DataFrame, input data containing only errors
-    # :param grouping_variables: list
-    :return: DataFrame, data with calculated accuracy metrics
+    :param df: DataFrame, processed input data
+    :return: DataFrame, data with calculated metrics
     """
     queries = df.groupby(by=['ID', 'version'])['response'].size().rename('queries')
     errors = df[df['error']].groupby(by=['ID', 'version'])['response'].count().rename('errors')
     accuracy_ratio = ((1 - errors / queries) * 100).rename('accuracy')
 
-    df_errors = pd.concat([queries, errors, accuracy_ratio], axis=1)
-    df_errors.fillna({'errors': 0, 'accuracy': 100}, inplace=True)
+    df_evaluation = pd.concat([queries, errors, accuracy_ratio], axis=1)
+    df_evaluation.fillna({'errors': 0, 'accuracy': 100}, inplace=True)
     print(
-        f"{df_errors}"
+        f"{df_evaluation}"
     )
 
-    return df_errors
+    return df_evaluation
 
 
-def compute_models_accuracy(df):
+def compute_model_accuracy(df, model):
     """
-    :param df:
-    :return: DataFrame, data with calculated accuracy metrics
+    Calculate the number of queries, errors, and accuracy percentage by participant and by task version.
+    :param df: DataFrame, processed input data
+
+    :param model: str, name of the recognition model to compute accuracy for
+    :return: DataFrame, data with calculated metrics
     """
     queries = df.groupby(by=['ID'])['response'].size().rename('queries')
-    errors_speech = df[df['response'] == 'error_speech'].groupby(by=['ID'])['response'].count().rename('errors_speech')
-    errors_gesture = df[df['response'] == 'error_gesture'].groupby(by=['ID'])['response'].count().rename('errors_gesture')
+    errors = df[df['response'] == f'error_{model}'].groupby(by=['ID'])['response'].count().rename(f'errors_{model}')
 
-    accuracy_gesture = ((1 - errors_gesture / queries) * 100).rename('accuracy_gesture')
-    accuracy_speech = ((1 - errors_speech / queries) * 100).rename('accuracy_speech')
+    accuracy = ((1 - errors / queries) * 100).rename(f'accuracy_{model}')
 
-    df_errors = pd.concat([queries, errors_gesture, accuracy_gesture, errors_speech, accuracy_speech], axis=1)
-    df_errors.fillna({'errors_gesture': 0, 'errors_speech': 0, 'accuracy_gesture': 100, 'accuracy_speech': 100},
-                     inplace=True)
+    df_evaluation = pd.concat([queries, errors, accuracy], axis=1)
+    df_evaluation.fillna({f'errors_{model}': 0, f'accuracy_{model}': 100},inplace=True)
     print(
-        f"{df_errors}"
+        f"{df_evaluation}"
     )
 
-    return df_errors
+    return df_evaluation
 
 
 def print_stats_accuracy(df, accuracy_variable, subject):
     """
     Print the mean and standard deviation of the accuracy for a choosen subject.
-    :param df: DataFrame of the analysis subject
+    :param df: DataFrame, with metrics for the evaluation
     :param accuracy_variable: string, name of the df's column containing the accuracy values
     :param subject: string, name of the subject to print
     """
     print(
-        f"\nMean accuracy in {subject}: {round(df[accuracy_variable].mean(), 3)} %"
-        f"\nStandard deviation of accuracy in {subject}: {round(df[accuracy_variable].std(), 3)} %"
+        f"\nMean accuracy in {subject}: {round(df[accuracy_variable].mean(), 2)}%"
+        f"\nStandard deviation of accuracy in {subject}: {round(df[accuracy_variable].std(), 2)}%"
     )
 
 
-def run_ttest(ttest_df, accuracy_variable1, accuracy_variable2):
+def run_tasks_ttest(ttest_df, accuracy_variable1, accuracy_variable2):
     """
     Perform a paired t-test between the two accuracy variables and print the result.
-    :param ttest_df: DataFrame, with accuracies values to compare in relative columns
+    :param ttest_df: DataFrame, with metrics for the evaluation
+    :param accuracy_variable1: str, name of the column in the df containing the accuracy values of the first group
+    :param accuracy_variable2: str, name of the column in the df containing the accuracy values of the second group
     """
     t_stat, p_value = ttest_rel(ttest_df[accuracy_variable1], ttest_df[accuracy_variable2])
-    if p_value < 0.05:
-        print(f"\nThe difference is statistically significant: t = {round(t_stat, 3)}, p = {round(p_value, 3)}")
-    else:
-        print(f"\nThe difference is not statistically significant: t = {round(t_stat, 3)}, p = {round(p_value, 3)}")
+    significance = "statistically significant" if p_value < 0.05 else "not statistically significant"
+    print(f"\nThe difference between the two groups accuracies is {significance}: t = {round(t_stat, 2)}, p = {round(p_value, 2)}")
+
+
+def run_model_ttest(ttest_df, accuracy_variable, popmean=95):
+    """
+    Perform a one sample t-test between the model accuracy and a benchmark (population) value.
+    :param ttest_df: DataFrame, with metrics for the evaluation
+    :param accuracy_variable: str, name of the column in the df containing the accuracy values
+    :param popmean: float, population mean to use as benchmark value for comparison
+    """
+    t_stat, p_value = ttest_1samp(ttest_df[accuracy_variable], popmean=popmean, alternative='less')
+    significance = "statistically lower than" if p_value < 0.05 else "not statistically lower than"
+    print(f"\nThe accuracy is {significance} {popmean}% : t = {round(t_stat, 2)}, p = {round(p_value, 2)}")
 
 
 def test_accuracy_difference_tasks(df):
     """
     Test for differences in accuracy between the two task versions.
-    :param df: DataFrame, data containing only errors
+    :param df: DataFrame, processed input data
+
     """
-    df_errors = compute_tasks_accuracy(df)
-    ttest_df = df_errors.reset_index().pivot(index='ID', columns='version', values='accuracy')
+    df_evaluation = compute_tasks_accuracy(df)
+    ttest_df = df_evaluation.reset_index().pivot(index='ID', columns='version', values='accuracy')
 
     print_stats_accuracy(ttest_df, 'speech', 'speech commands task')
     print_stats_accuracy(ttest_df, 'keys', 'key presses task')
-    run_ttest(ttest_df, 'keys', 'speech')
+    run_tasks_ttest(ttest_df, 'keys', 'speech')
 
 
-def test_accuracy_difference_models(df):
+def test_accuracy_significance_model(df, model):
     """
-    Test for differences in accuracy between the models of speech and gesture recognition.
-    :param df: DataFrame, data containing only errors
+    Test for level of accuracy of the recognition model.
+    :param df: DataFrame, processed input data
+
+    :param model: str, name of the model to test
     """
-    df_errors = compute_models_accuracy(df)
+    df_evaluation = compute_model_accuracy(df, model)
 
-    print_stats_accuracy(df_errors, 'accuracy_speech', 'speech recognition model')
-    print_stats_accuracy(df_errors, 'accuracy_gesture', 'gesture recognition model')
-    run_ttest(df_errors, 'accuracy_gesture', 'accuracy_speech')
+    print_stats_accuracy(df_evaluation, f'accuracy_{model}', f'{model} recognition model')
+    run_model_ttest(df_evaluation, f'accuracy_{model}')
 
 
-def compare_task_accuracies_all_errors():
+def compare_task_accuracies_all_errors(df):
+    """
+    :param df: DataFrame, processed input data
+    Compare accuracy of the two evaluation task versions.
+    """
     print("#### COMPARE TASK VERSIONS: SPEECH COMMANDS VS. KEYS PRESSES - CONSIDERING ALL POSSIBILE ERRORS ####")
 
-    # Load data
-    df = accuracy_evaluation_df()
-
-    # Inspect data properties
+    # Print relevant information
     print(f"\nTotal queries: {len(df)}"
           f"\nError types: {', '.join([et.split('_')[1] for et in df[df['error']]['response'].unique()])}.")
 
@@ -140,14 +154,17 @@ def compare_task_accuracies_all_errors():
     test_accuracy_difference_tasks(df)
 
 
-def compare_task_accuracies_specific_errors():
+def compare_task_accuracies_specific_errors(df):
+    """
+    :param df: DataFrame, processed input data
+    Compare accuracy of the two evaluation task versions, only considering error types specific to the tasks.
+    """
     print("#### COMPARE TASK VERSIONS: SPEECH COMMANDS VS. KEYS PRESSES - CONSIDERING ONLY TASK-SPECIFIC ERRORS ####")
 
-    # Load data
-    df = accuracy_evaluation_df()
+    # Filter error responses to those specific for the task versions
     df = df[~df['error'] | df['response'].isin(['error_speech', 'error_keys'])]
 
-    # Inspect data properties
+    # Print relevant information
     print(f"\nTotal queries: {len(df)}"
           f"\nError types: {', '.join([et.split('_')[1] for et in df[df['error']]['response'].unique()])}.")
 
@@ -155,40 +172,79 @@ def compare_task_accuracies_specific_errors():
     test_accuracy_difference_tasks(df)
 
 
-def compare_models_accuracies():
-    print("#### COMPARE MODELS: SPEECH RECOGNITION VS. GESTURE RECOGNITION ####")
+def evaluate_model_accuracy(df, model):
+    """
+    Evaluate accuracy of a single recognition model.
+    :param df: DataFrame, processed input data
+    :param model: str, name of the model to evaluate
+    """
+    # if model == 'object':
+    #     df['response'] = df['response'].apply(lambda x: 'error_object' if x == 'error_objcolor' else x)
 
-    # Load data
-    df = accuracy_evaluation_df()
+    # Drop rows containing errors not specific to the model
+    df = df[~df['error'] | df['response'].isin([f'error_{model}'])]
 
-    # Drop rows containing errors related to speech or gesture
-    df = df[~df['error'] | df['response'].isin(['error_speech', 'error_gesture'])]
+    # Assign the same value to all correct responses
+    df.loc[~df['response'].isin([f'error_{model}']), 'response'] = 'correct'
 
-    # Assign the same value to all responses that were correct
-    df.loc[~df['response'].isin(['error_speech', 'error_gesture']), 'response'] = 'correct'
-
-    # Inspect data properties
+    # Print relevant information
     print(f"\nTotal queries: {len(df)}"
           f"\nError types: {', '.join([et.split('_')[1] for et in df[df['error']]['response'].unique()])}.")
 
-    # Test for differences in accuracy between the two versions
-    test_accuracy_difference_models(df)
+    # Compute statistics for the accuracy of the model for differences in accuracy between the two versions
+    test_accuracy_significance_model(df, model)
+
+
+def evaluate_gesture_model_accuracy(df):
+    """
+    :param df: DataFrame, processed input data
+    Evaluate accuracy of the gesture recognition model.
+    """
+    print("#### EVALUATE MODEL: GESTURE RECOGNITION ####")
+    evaluate_model_accuracy(df=df, model='gesture')
+
+
+def evaluate_speech_model_accuracy(df):
+    """
+    :param df: DataFrame, processed input data
+    Evaluate accuracy of the speech recognition model.
+    """
+    print("#### EVALUATE MODEL: SPEECH RECOGNITION ####")
+    evaluate_model_accuracy(df=df, model='speech')
+
+
+def evaluate_object_model_accuracy(df):
+    """
+    :param df: DataFrame, processed input data
+    Evaluate accuracy of the object recognition model.
+    """
+    print("#### EVALUATE MODEL: OBJECT RECOGNITION ####")
+    evaluate_model_accuracy(df=df, model='object')
 
 
 def main():
+    # Load the evaluation data
+    df = accuracy_evaluation_df()
+
     # Run statistical analysis to compare the overall accuracy of the system in the two task versions
     print(f"\n\n")
-    compare_task_accuracies_all_errors()
+    compare_task_accuracies_all_errors(df)
 
     # Run statistical analysis to compare the specific accuracy of each task version
     print(f"\n\n")
-    compare_task_accuracies_specific_errors()
+    compare_task_accuracies_specific_errors(df)
 
-    # Run statistical analysis to compare the accuracies of speech and gesture recognition models
+    # Run analysis to inspect accuracy of gesture recognition model
     print(f"\n\n")
-    compare_models_accuracies()
+    evaluate_gesture_model_accuracy(df)
 
+    # Run analysis to inspect accuracy of speech recognition model
+    print(f"\n\n")
+    evaluate_speech_model_accuracy(df)
 
+    # Run analysis to inspect accuracy of object recognition model
+    print(f"\n\n")
+    evaluate_object_model_accuracy(df)
 
 
 if __name__ == "__main__":
